383c7c2d8c1c2c13effa80d62a0c79eb535a7e46
Dajoun1 <119526266+Dajoun1@users.noreply.github.com>
2023-09-19 11:18:52 -0500

    added intializer folder into git folder

---
+++ factory-layout/src/test/java/com/accesspoint/factorylayout/FactoryLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesspoint.factorylayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
