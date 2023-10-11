21f1122a01444d0b69de53f88d1ac6d18163add2
Madilyn Carpenter <mcarpenter42@my.stlcc.edu>
2023-10-11 15:39:52 -0500

    Added some stuff to nav bar

--- factory-layout/layout-frontend/src/components/SideBar.js
+++ factory-layout/layout-frontend/src/components/SideBar.js
@@ -1,40 +1,31 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ColoredLine from './ColoredLine';
-import { MdSpaceDashboard ,MdOutlineRule  } from "react-icons/md";
+import { MdSpaceDashboard } from "react-icons/md";
 import { BsFillPieChartFill, BsTools } from "react-icons/bs";
-import {RiRobot2Fill} from "react-icons/ri"
-import { TfiLayoutGrid3Alt } from "react-icons/tfi";
+import { RiRobot2Fill } from "react-icons/ri";
 
-import './Sidebar.css'
+import './Sidebar.css';
 
+import React from 'react';
 
-import React, {useState, useEffect, useRef} from 'react';
 function SideBar() {
+  return (
+    <Card className='side-bar' bg='dark' text='light'>
+      <div>
+        <Card.Title><RiRobot2Fill style={{fontSize: '20px' }}/> Capstone Project</Card.Title>
+        <ColoredLine color='white' />
+        <Button className="icon-button"><MdSpaceDashboard style={{ fontSize: '25px' }} />Dashboard</Button>
+        <Button className="icon-button"><BsFillPieChartFill style={{ fontSize: '25px' }} />Reports</Button>
+        <Button className="icon-button"><BsTools style={{ fontSize: '25px' }} />Simulation</Button>
+      </div>
 
-
-
-
-
-
-return (
-    
-  <Card className='side-bar ' bg='dark' text='light' >
-    <div>
-      <Card.Title><RiRobot2Fill/> Capstone Project</Card.Title>
-      <ColoredLine color='white'/>
-      <Button ><MdSpaceDashboard/> Dashboard</Button>
-      <Button variant="dark"><BsTools/> Simulation</Button>
-      <Button variant="dark"><BsFillPieChartFill/> Reports</Button>
-    </div>
-
-    <div>
-      <ColoredLine color='white'/>
-      <Button variant="dark" >Signout</Button>
-    </div>
-  </Card>
-
-    
-)
+      <div>
+        <ColoredLine color='white' />
+        <Button variant="dark">Sign Out</Button>
+      </div>
+    </Card>
+  );
 }
+
 export default SideBar;
