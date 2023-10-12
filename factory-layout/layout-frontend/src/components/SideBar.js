ba34a22a668b118752228681ab878d30f51da4fc
Madilyn Carpenter <mcarpenter42@my.stlcc.edu>
2023-10-12 11:50:16 -0500

    Added on click event for highlighting effect

--- factory-layout/layout-frontend/src/components/SideBar.js
+++ factory-layout/layout-frontend/src/components/SideBar.js
@@ -1,40 +1,53 @@
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ColoredLine from './ColoredLine';
-import { MdSpaceDashboard   } from "react-icons/md";
+import { MdSpaceDashboard } from "react-icons/md";
 import { BsFillPieChartFill, BsTools } from "react-icons/bs";
-import {RiRobot2Fill} from "react-icons/ri"
+import { RiRobot2Fill } from "react-icons/ri";
+import './Sidebar.css';
 
-
-import './Sidebar.css'
-
-
-import React, {useState, useEffect, useRef} from 'react';
 function SideBar() {
+  const [selectedButton, setSelectedButton] = useState('dashboard');
 
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
 
+  const isButtonSelected = (buttonId) => selectedButton === buttonId;
 
+  return (
+    <Card className='side-bar' bg='dark' text='light'>
+      <div>
+        <Card.Title><RiRobot2Fill />Capstone Project</Card.Title>
+        <ColoredLine color='white' />
 
+        <Button
+          onClick={() => handleButtonClick('dashboard')}
+          variant={isButtonSelected('dashboard') ? 'primary' : 'dark'}>
+        <MdSpaceDashboard viewBox="3 2 19 19" />Dashboard
+        </Button>
 
+        <Button
+          onClick={() => handleButtonClick('simulation')}
+          variant={isButtonSelected('simulation') ? 'primary' : 'dark'}>
+          <BsTools />Simulation
+        </Button>
 
-return (
+        <Button
+          onClick={() => handleButtonClick('reports')}
+          variant={isButtonSelected('reports') ? 'primary' : 'dark'}>
+          <BsFillPieChartFill />Reports
+        </Button>
+      </div>
 
-  <Card className='side-bar ' bg='dark' text='light' >
-    <div>
-      <Card.Title><RiRobot2Fill/>Capstone Project</Card.Title>
-      <ColoredLine color='white'/>
-      <Button ><MdSpaceDashboard viewBox="3 2 19 19" />Dashboard</Button>
-      <Button  variant="dark"><BsTools/>Simulation</Button>
-      <Button  variant="dark"><BsFillPieChartFill/>Reports</Button>
-    </div>
-
-    <div>
-      <ColoredLine />
-      <Button variant="dark" >Sign Out</Button>
-    </div>
-  </Card>
-
-
-)
+      <div>
+        <ColoredLine />
+        <Button variant="dark">Sign Out</Button>
+      </div>
+    </Card>
+  );
 }
+
 export default SideBar;
+
