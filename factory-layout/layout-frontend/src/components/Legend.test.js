8df2333439c7c63355042a7716d2ec37dfe767ac
Madilyn Carpenter <mcarpenter42@my.stlcc.edu>
2023-10-24 14:02:37 -0500

    Legend test :)

--- factory-layout/layout-frontend/src/components/Legend.test.js
+++ factory-layout/layout-frontend/src/components/Legend.test.js
@@ -1,15 +1,35 @@
 import { render, screen } from '@testing-library/react';
-import LayoutConfigurationCard from './LayoutConfigurationCard';
+import Legend from './Legend';
 
 describe("Legend testing", () => {
-    test('Legend appears in dropdown', async () => {
-      render(<ColoredLine />)
-      const items = await screen.getByTestId('')
-      expect().toBeInTheDocument();
-    })
-    test('Legend ', async () => {
-          render(<ColoredLine color={''}/>)
-          const items = await screen.getByTestId('')
-          expect().toEqual('');
+
+    test('Legend is displayed on page', async () => {
+          render(<Legend/>)
+          const items = await screen.getByText('Legend:')
+          expect(items).toBeInTheDocument();
+        })
+
+    test('Open is displayed', async () => {
+          render(<Legend/>)
+          const items = await screen.getByText('Open')
+          expect(items).toBeInTheDocument();
+        })
+
+    test('Wall is displayed', async () => {
+          render(<Legend/>)
+          const items = await screen.getByText('Wall')
+          expect(items).toBeInTheDocument();
+        })
+
+    test('Robot is displayed', async () => {
+          render(<Legend/>)
+          const items = await screen.getByText('Robot')
+          expect(items).toBeInTheDocument();
+        })
+
+    test('End is displayed', async () => {
+          render(<Legend/>)
+          const items = await screen.getByText('End')
+          expect(items).toBeInTheDocument();
         })
 })
