c66e7cb5253d65d54ae2724fb02f3e96878669d2
Madilyn Carpenter <mcarpenter42@my.stlcc.edu>
2023-10-31 12:38:23 -0500

    Stuff

--- factory-layout/src/main/java/com/accesspoint/factorylayout/Layout.java
+++ factory-layout/src/main/java/com/accesspoint/factorylayout/Layout.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "layout")
+public
 class Layout {
     /*
     Field Properties
---
+++ factory-layout/src/main/java/com/accesspoint/factorylayout/repository/LayoutRepository.java
@@ -0,0 +1,13 @@
+/*package com.accesspoint.factorylayout.repository;
+
+import com.accesspoint.factorylayout.Layout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface LayoutRepository extends JpaRepository<Layout, Long> {
+
+}
+
+ */
