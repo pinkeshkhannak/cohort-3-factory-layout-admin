1912902e0018ede9bb66fba9cbb3a51186a50165
JoneyKroney <johnlowriefirst@gmail.com>
2023-11-06 13:29:22 -0600

    made cancle button link to dashboard page

--- factory-layout/layout-frontend/src/components/Settings.js
+++ factory-layout/layout-frontend/src/components/Settings.js
@@ -15,30 +15,34 @@ function Settings({ currentPage = "dashboard" }) {
             <ColoredLine width={"100%"} />
 
             <form>
-              <div>
-                <input type="name" className="btn btn-secondary" id="exampleInputEmail1" aria-describedby="layoutName" placeholder="Layout Name" />
-              </div>
+              <Stack gap={3}>
+                <div>
+                  <input
+                    type="name"
+                    className="btn btn-secondary"
+                    id="exampleInputEmail1"
+                    aria-describedby="layoutName"
+                    placeholder="Layout Name"
+                  />
+                </div>
 
-              <p></p>
+                <Dropdown>
+                  <Dropdown.Toggle variant="btn btn-secondary">Initial Facing Direction</Dropdown.Toggle>
 
-              <Dropdown>
-                <Dropdown.Toggle variant="btn btn-secondary" id="dropdown-basic-button">
-                  Initial Facing Direction
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  <Dropdown.Item>Facing North</Dropdown.Item>
-                  <Dropdown.Item>Facing East</Dropdown.Item>
-                  <Dropdown.Item>Facing South</Dropdown.Item>
-                  <Dropdown.Item>Facing West</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Facing North</Dropdown.Item>
+                    <Dropdown.Item>Facing East</Dropdown.Item>
+                    <Dropdown.Item>Facing South</Dropdown.Item>
+                    <Dropdown.Item>Facing West</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Stack>
             </form>
-            <p></p>
+
             <div className="save-cancel">
-              <button type="button" className="btn btn-secondary">
+              <Button href="Dashboard" variant="dark">
                 Cancel
-              </button>
+              </Button>
               <button type="button" className="btn btn-primary">
                 Save
               </button>
