2db4c8c26adb84b99253f48938a6778970d5ddd3
JoneyKroney <johnlowriefirst@gmail.com>
2023-11-07 16:02:04 -0600

    added stack for spacing for settings card

--- factory-layout/layout-frontend/src/components/Settings.js
+++ factory-layout/layout-frontend/src/components/Settings.js
@@ -8,8 +8,8 @@ import React, { useState } from "react";
 import LayoutConfigurationCard from "./LayoutConfigurationCard";
 
 function Settings() {
-    const [layoutName, setLayoutName] = useState("");
-    const [facingDirection, setFacingDirection] = useState(null);
+  const [layoutName, setLayoutName] = useState("");
+  const [facingDirection, setFacingDirection] = useState(null);
 
   // Function to handle changes in layout name
   const handleLayoutNameChange = (e) => {
@@ -30,50 +30,64 @@ function Settings() {
             <ColoredLine width={"100%"} />
 
             <form>
-              <div>
-                <input type="text" className="btn btn-secondary" id="exampleInputEmail1" aria-describedby="layoutName" placeholder="Layout Name" value={layoutName} onChange={handleLayoutNameChange} />
-              </div>
+              <Stack gap={3}>
+                <div>
+                  <input
+                    type="text"
+                    className="btn btn-secondary"
+                    id="exampleInputEmail1"
+                    aria-describedby="layoutName"
+                    placeholder="Layout Name"
+                    value={layoutName}
+                    onChange={handleLayoutNameChange}
+                  />
+                </div>
 
-                
+                <Dropdown>
+                  <Dropdown.Toggle variant="btn btn-secondary" id="dropdown-basic-button">
+                    {facingDirection ? facingDirection : "Initial Facing Direction"}
+                  </Dropdown.Toggle>
 
-              <Dropdown>
-                <Dropdown.Toggle variant="btn btn-secondary" id="dropdown-basic-button" >
-                  {(facingDirection)?facingDirection:"Initial Facing Direction"}
-                </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item
+                      onClick={() => {
+                        handleFacingDirectionChange("North");
+                      }}
+                    >
+                      North
+                    </Dropdown.Item>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item
-                  onClick={() => {
-                     handleFacingDirectionChange("North");
-                  }}
-                  >North</Dropdown.Item>
+                    <Dropdown.Item
+                      onClick={() => {
+                        handleFacingDirectionChange("East");
+                      }}
+                    >
+                      East
+                    </Dropdown.Item>
 
-                  <Dropdown.Item
-                  onClick={() => {
-                     handleFacingDirectionChange("East");
-                                    }}
-                  >East</Dropdown.Item>
+                    <Dropdown.Item
+                      onClick={() => {
+                        handleFacingDirectionChange("South");
+                      }}
+                    >
+                      South
+                    </Dropdown.Item>
 
-                  <Dropdown.Item
-                  onClick={() => {
-                     handleFacingDirectionChange("South");
-                                    }}
-                  >South</Dropdown.Item>
-
-                  <Dropdown.Item
-                  onClick={() => {
-                      handleFacingDirectionChange("West");
-                                    }}
-                  >West</Dropdown.Item>
-
-                </Dropdown.Menu>
-              </Dropdown>
+                    <Dropdown.Item
+                      onClick={() => {
+                        handleFacingDirectionChange("West");
+                      }}
+                    >
+                      West
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Stack>
             </form>
-            <br/>
+            <br />
             <div className="save-cancel">
               <Button href="Dashboard" variant="dark">
                 Cancel
-
               </Button>
               <button type="button" className="btn btn-primary">
                 Save
@@ -84,11 +98,10 @@ function Settings() {
       </div>
 
       <settingsInfo
-      layoutName={layoutName} // Pass layoutName to LayoutConfigurationCard
-      facingDirection={facingDirection} // Pass facingDirection to LayoutConfigurationCard
-     />
-
+        layoutName={layoutName} // Pass layoutName to LayoutConfigurationCard
+        facingDirection={facingDirection} // Pass facingDirection to LayoutConfigurationCard
+      />
     </Card>
   );
 }
-  export default Settings;
+export default Settings;
