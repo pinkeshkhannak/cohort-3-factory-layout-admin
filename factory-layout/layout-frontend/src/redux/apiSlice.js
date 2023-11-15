c316eb1e4b6e19d190ad37c69927363e89abdaf1
JoneyKroney <johnlowriefirst@gmail.com>
2023-11-15 09:07:46 -0600

    maybe api stuff not really working

--- factory-layout/layout-frontend/src/redux/apiSlice.js
+++ factory-layout/layout-frontend/src/redux/apiSlice.js
@@ -1,27 +1,20 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-// const api = createApi({
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "/api/layouts",
-//   }),
-//   tagTypes: ["Post"],
-//   endpoints: (build) => ({
-//     // The query accepts a number and returns a Post
-//     getPost: build.query({
-//       // note: an optional `queryFn` may be used in place of `query`
-//       query: () => ({ url: `save` }),
-//       // Pick out data and prevent nested properties in a hook or selector
-//       transformResponse: (response, meta, arg) => response.data,
-//       // Pick out errors and prevent nested properties in a hook or selector
-//       transformErrorResponse: (response, meta, arg) => response.status,
-//       providesTags: (result, error, id) => [{ type: "Post", id }],
-//       // The 2nd parameter is the destructured `QueryLifecycleApi`
-//       async onQueryStarted(arg, { dispatch, getState, extra, requestId, queryFulfilled, getCacheEntry, updateCachedData }) {},
-//       // The 2nd parameter is the destructured `QueryCacheLifecycleApi`
-//       async onCacheEntryAdded(
-//         arg,
-//         { dispatch, getState, extra, requestId, cacheEntryRemoved, cacheDataLoaded, getCacheEntry, updateCachedData }
-//       ) {},
-//     }),
-//   }),
-// });
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8081/api/layouts" }),
+  endpoints: (build) => ({
+    updateTodos: build.mutation({
+      query: () => ({
+        url: "/save",
+        method: "POST",
+        body: {},
+      }),
+    }),
+  }),
+});
+
+// Action creators are generated for each case reducer function
+export const { useUpdateTodosMutation } = apiSlice;
+
+export default SettingsSlice.reducer;
